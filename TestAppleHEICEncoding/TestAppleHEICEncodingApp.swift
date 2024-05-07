d574da8fba970f3b60618262beaea060aa08fdf5
lizhuoli <lizhuoli@bytedance.com>
2024-05-07 18:54:53 +0800

    Initial Commit

---
+++ TestAppleHEICEncoding/TestAppleHEICEncodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppleHEICEncodingApp.swift
+//  TestAppleHEICEncoding
+//
+//  Created by lizhuoli on 2024/5/7.
+//
+
+import SwiftUI
+
+@main
+struct TestAppleHEICEncodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
